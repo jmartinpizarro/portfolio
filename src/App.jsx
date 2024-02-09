18513395167a482500898f22f17ddc5bbe261016
Javier Martin <jmartinpizarro04@gmail.com>
2024-02-09 13:53:41 +0100

    init of presentation section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import FollowMouse from './FollowMouse';
 import Navbar from './Navbar';
 import TypingEffect from './TypingEffect';
 
 function App(){
+  const [startSecondTyping, setStartSecondTyping] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setStartSecondTyping(true)
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return(
     <>
       <FollowMouse></FollowMouse>
       <Navbar></Navbar>
-      <TypingEffect phrase={'Soy Javier Martín, desarrollador web FrontEnd y creador de contenido'}></TypingEffect>
-      <img src="./src/assets/fotoJMP.jpg" alt="" />
+      <section className='initContainer'>
+        <img className='profile' src="./src/assets/fotoJMP.jpg" alt="Javier Martin perfil" />
+        <h2 className="intro">Hola, soy Javier Martín</h2>
+        {startSecondTyping && <TypingEffect phrase={'Segundo TypingEffect que comienza después de 3 segundos'} />}
+      </section>
     </>
   )
 }
