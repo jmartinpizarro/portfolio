70e9ef112e962e23058534bb6e2759de00eb10e8
Javier Martin <jmartinpizarro04@gmail.com>
2024-02-09 07:55:43 +0100

    comp. mouse follower

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import FollowMouse from './FollowMouse';
 import Navbar from './Navbar';
 import TypingEffect from './TypingEffect';
 
 function App(){
   return(
     <>
+      <FollowMouse></FollowMouse>
       <Navbar></Navbar>
       <TypingEffect phrase={'Soy Javier Martín, desarrollador web FrontEnd y creador de contenido'}></TypingEffect>
     </>
---
+++ src/FollowMouse.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+
+function FollowMouse(){
+    const [position, setPosition] = useState({x: 0, y: 0})
+
+    useEffect(() => {
+        const handleMove = (event) => {
+            const {clientX, clientY} = event
+            setPosition({x: clientX, y: clientY})
+        }
+
+        window.addEventListener('pointermove', handleMove)
+    }, [])
+
+    return (
+        <>
+            <div style={{
+                position: 'absolute',
+                backgroundColor: 'white',
+                border: 'none',
+                borderRadius: '50%',
+                opacity: 0.8,
+                pointerEvents: 'none',
+                left: position.x - 10 + 'px',
+                top: position.y - 10 + 'px',
+                width: 10,
+                height: 10,
+                transform: 'translate(0px, 0px)'}}
+            />
+        </>
+    )
+}
+
+export default FollowMouse
