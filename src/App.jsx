a2ae198869ca11545902b31b3443dfc3af041332
Javier Martin <jmartinpizarro04@gmail.com>
2024-02-18 17:02:29 +0100

    Finish section contact

--- src/App.jsx
+++ src/App.jsx
@@ -106,7 +106,24 @@ function App(){
         </div>
       </section>
 
-      <section id='contact' className='contact'></section>
+      <section id='contact' className='contact'>
+        <h1 className="info">¿Quieres que me ponga manos a la obra?</h1>
+        <form className="contact-form">
+          <div className="form-group">
+            <label htmlFor="name">Nombre:</label>
+            <input type="text" id="name" name="name" required/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Correo electrónico:</label>
+            <input type="email" id="email" name="email" required/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Mensaje:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+          </div>
+          <button type="submit" className="contact-btn">Haz click, ¡que no muerdo!</button>
+        </form>
+      </section>
 
     </> 
   )
